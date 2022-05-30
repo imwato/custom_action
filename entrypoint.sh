69e5c3c4c9f831512b421bbd0b0e7db45c646c33
William Alexander <alextoki@gmail.com>
2022-05-30 16:08:47 +1200

    ..

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ then
         echo "## [TESTING] Keyword was found but no release was created."
     else
         #echo $DATA | http POST $URL | jq .
-        echo $DATA | curl -X POST -H "Authorization: ${GITHUB_TOKEN}" $URL | jq .
+        echo $DATA | curl -X POST -H "Authorization: ${GITHUB_TOKEN} $URL" | jq .
     fi
 # otherwise
 else
